10b86af1ef2290963be085af02ef52e7f5e4e083
bendikolsen <bendikolsen@gmail.com>
2019-10-08 10:13:53 +0200

    fixed button size and background opacity bug.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 
 // Total registered
-let totalCount = 3;
+let totalCount = 250;
 
 app.get("/", (req, res) => {
     let querytotalWlCount = 'SELECT COUNT(*) AS total FROM users WHERE whitelisted = TRUE ';
