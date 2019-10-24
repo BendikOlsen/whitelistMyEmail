f38033047215ccf183b6c19ba135fd6d9b53290c
bendikolsen <bendikolsen@gmail.com>
2019-10-24 10:27:49 +0200

    fixed port settings

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ app.post('/register', (req, res) => {
 
 
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log('App listening on port 3000!')
+app.listen(process.env.PORT || 5000, () => {
+    console.log('App listening on port 5000!')
 });
 
